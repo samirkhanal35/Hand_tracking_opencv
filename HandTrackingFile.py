42dac8daf0b080585a854a81abf3a32c2a3f69b9
samirkhanal35 <samirkhanal35@gmail.com>
2022-11-23 23:46:42 -0500

    Getting the tip values

--- HandTrackingFile.py
+++ HandTrackingFile.py
@@ -16,16 +16,26 @@ while True:
     img = cv2.flip(img, 1)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    # print(results.multi_hand_landmarks)
+    print("-----------Results----------")
+    thumb_tip = 4
+    index_finger_tip = 8
     if results.multi_hand_landmarks:
+        thumb_tip_landmark = {}
+        index_finger_tip_landmark = {}
         for handlms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
 
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
+        # getting the tip values
+        thumb_tip_landmark = handlms.landmark[thumb_tip]
+        index_finger_tip_landmark = handlms.landmark[index_finger_tip]
 
-    cv2.putText(img, str(int(fps)),(10,70), cv2.FONT_HERSHEY_SIMPLEX, 2,(255,0,255),2)
+
+        print("tip of thumb:", thumb_tip_landmark)
+        print("tip of index finger:", index_finger_tip_landmark)
+
+
+
+    # cv2.putText(img, str(int(fps)),(10,70), cv2.FONT_HERSHEY_SIMPLEX, 2,(255,0,255),2)
 
 
     cv2.imshow("img", img)
